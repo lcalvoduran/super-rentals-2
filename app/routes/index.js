3e27613e278fad65c78c68c1033ec6808e5b1285
Leandro Calvo <lcalvo.intern@copyright.com>
2022-06-29 13:55:47 +0200

    last commit chapter 1

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,34 +1,24 @@
 import Route from '@ember/routing/route';
 const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 export default class IndexRoute extends Route {
-
-   
-/*     async model() {
-    return {
-      title: 'Grand Old Mansion',
-      owner: 'Veruca Salt',
-      city: 'San Francisco',
-      location: {
-        x: 3,
-        y: 2,
-        z: 4,
-      },
-      category: 'Estate',
-      type: 'Standalone',
-      bedrooms: 15,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-      description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-    }; 
-  } */
-
       
  async model(){   
 
     let response = await fetch('../api/rentals.json');
-    let parsed = await response.json();
-    console.log(parsed)
-    return parsed; 
+    let { data } = await response.json();
 
-    }
+    return data.map((model) => {
+      let { attributes } = model;
+      let type;
+
+      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+        type = 'Community';
+      } else {
+        type = 'Standalone';
+      }
+
+      return { type, ...attributes };
+    });
+  }
     
 }
